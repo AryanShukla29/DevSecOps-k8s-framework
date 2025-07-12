fe08c19079dc348ad0d6dda364bbbe5b42caafb9
AryanShukla29 <119594508+AryanShukla29@users.noreply.github.com>
2025-07-12 09:07:42 +0000

    latest changes

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -1,27 +1,29 @@
-provider "aws" {
-    region = "us-east-1"
-}
-
 resource "aws_instance" "jenkins" {
-    ami = "ami-020cba7c55df1f615"
-    instance_type="t2.micro"
-    key_name =var.key_name
+  ami           = "ami-020cba7c55df1f615"  # Ubuntu 22.04 LTS (us-east-1)
+  instance_type = "t2.micro"
+  key_name      = var.key_name
 
-tags={
-    Name="Jenkins-Server"
-}
+  tags = {
+    Name = "Jenkins-Server"
+  }
 
-provisioner "remote-exec" {
-    inline =[
-        "sudo apt update",
-        "sudo apt install -y docker.io",
-        "sudo usermodm -aG docker ubuntu"
+  # (optional: add EBS size increase if needed)
+  root_block_device {
+    volume_size = 16
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update",
+      "sudo apt install -y docker.io",
+      "sudo usermod -aG docker ubuntu"
     ]
+
     connection {
-        type ="ssh"
-        user ="ubuntu"
-        private_key=file(var.private_key_path)
-        host = self.public_ip
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file(var.private_key_path)
+      host        = self.public_ip
     }
-}
+  }
 }
--- terraform/aws/output.tf
+++ terraform/aws/output.tf
@@ -1,4 +1,4 @@
-output " jenkins_public_ip"{
-    value= aws_instance.jenkins.jenkins_public_ip
-    description="Public IP of the Jenkins server"
+output "jenkins_public_ip" {
+  value = aws_instance.jenkins.public_ip
+  description = "Public IP of the Jenkins server"
 }
